397e61ed60c8118087898bc3eb1330f675c1b8e5
hveficent <hector.villarreal@eficent.com>
2019-01-04 11:00:18 +0100

    Add stock_change_product_reason security

--- stock_change_qty_reason/__manifest__.py
+++ stock_change_qty_reason/__manifest__.py
@@ -14,6 +14,7 @@
         'stock',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/base_config_view.xml',
         'views/stock_change_product_reason_view.xml',
         'wizard/stock_product_change_qty.xml'
